22f0d1e5a03bad8ef448af164893e938e60d1ece
鼠笑天 <zhuchuangang20@163.com>
2017-11-22 23:00:05 +0800

    kubeadm

--- kubeadm/v1.8.4/kubeadm-reset.sh
+++ kubeadm/v1.8.4/kubeadm-reset.sh
@@ -11,3 +11,5 @@ rpm -e --nodeps kubectl-1.8.4-0
 rpm -e --nodeps kubelet-1.8.4-0
 rpm -e --nodeps kubernetes-cni-0.5.1-1
 rpm -e --nodeps socat-1.7.3.2-2.el7
+# 卸载docker
+#yum remove -y docker-engine-1.12.6-1.el7.centos.x86_64
